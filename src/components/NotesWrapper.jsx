af6d0383858817c60bd3033606ddab6c2fbfd684
athdromeda <anixatho01@gmail.com>
2023-05-17 20:42:23 +0700

    style: [Layout] Responsive Layout

--- src/components/NotesWrapper.jsx
+++ src/components/NotesWrapper.jsx
@@ -1,20 +1,51 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from 'react';
 import { Children } from 'react';
 
 const NotesWrapper = ({ children }) => {
-  const oddCards = Children.toArray(children).filter(
-    (_, index) => index % 2 === 0
-  );
-  const evenCards = Children.toArray(children).filter(
-    (_, index) => index % 2 !== 0
-  );
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const column = getColumnsCount(windowWidth);
+  const cardsColumn = generateBlankArrays(column);
+
+  for (let i = 0; i < column; i++) {
+    cardsColumn[i].push(
+      Children.toArray(children).filter((_, index) => index % column === i)
+    );
+  }
 
   return (
-    <div className="m-6 mt-16 grid grid-cols-2 gap-4">
-      <div>{oddCards}</div>
-      <div>{evenCards}</div>
+    <div className={'m-6 mt-16 grid gap-4 grid-cols-' + column}>
+      {cardsColumn.map((e, i) => (
+        <div key={i}>{e}</div>
+      ))}
     </div>
   );
 };
+
+function generateBlankArrays(count) {
+  return Array.from({ length: count }, () => []);
+}
+
+function getColumnsCount(width) {
+  if (width < 550) {
+    return 2;
+  }
+  if (width < 800) {
+    return 3;
+  }
+  return 4;
+}
 
 export default NotesWrapper;
